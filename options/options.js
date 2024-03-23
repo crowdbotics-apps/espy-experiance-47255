5211976a1b9d85c2e6c2e006f2be45549b20e1a0
Crowdbotics <team@crowdbotics.com>
2024-03-23 07:02:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "espy-experiance-47255",
+  url: "https://espy-experiance-47255.botics.co",
+  api: "https://espy-experiance-47255.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
